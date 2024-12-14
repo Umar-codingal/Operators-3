3082b685925d85797d427eb9a43d74a5de337cfd
Umar-codingal <umarfarukpatelkp@gmail.com>
2024-12-14 09:24:44 +0530

    Add files via upload

---
+++ identityOperator.py
@@ -0,0 +1,15 @@
+a= 20
+b= 30
+
+if a is b:
+    print("a & b has the same identity")
+else:
+    print("They are diffrent")
+
+a=20
+b=20
+
+if a is not b:
+     print("They are diffrent")
+else:
+   print("a & b has the same identity")
